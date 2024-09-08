f03674f11959137cb1a8a534132b9400e57ac720
kuma.jung <yj.jung5078@gmail.com>
2024-09-08 14:59:16 +0900

    modify file name

--- terraform/iam/zerone-id/remote_state.tf
+++ terraform/iam/zerone-id/remote_state.tf
@@ -11,8 +11,8 @@ data "terraform_remote_state" "demo_tmcdapne2" {
 }
 
 
-data "terraform_remote_state" "codedeploy" {
-  backend = "s3"
-  config  = merge(var.remote_state.codedeploy.zerone-id.apne2)
-  # config = merge(var.remote_state.codedeploy.zerone-id.apne2, {"assume_role" = {"role_arn" = var.assume_role_arn}})
-}
+# data "terraform_remote_state" "codedeploy" {
+#   backend = "s3"
+#   config  = merge(var.remote_state.codedeploy.zerone-id.apne2)
+#   # config = merge(var.remote_state.codedeploy.zerone-id.apne2, {"assume_role" = {"role_arn" = var.assume_role_arn}})
+# }
