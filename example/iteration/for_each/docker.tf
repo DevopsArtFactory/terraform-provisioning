7a01ea2df9681b214423f031da44a5b16107ee5f
kuma.jung <yj.jung5078@gmail.com>
2024-09-12 13:18:23 +0900

    Add network mode

--- example/iteration/for_each/docker.tf
+++ example/iteration/for_each/docker.tf
@@ -7,6 +7,7 @@ resource "docker_container" "nginx" {
   for_each = { for port in var.ports : port.container_name => port }
   image = docker_image.nginx.image_id
   name  = each.key
+  network_mode = "bridge"
   ports {
     internal = 80
     external = each.value.port
