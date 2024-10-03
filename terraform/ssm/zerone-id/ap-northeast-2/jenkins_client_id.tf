79fba2604f8477653c94e9d5a2231dab3f51a8a5
kuma.jung <yj.jung5078@gmail.com>
2024-10-03 14:34:18 +0900

    Test:

--- terraform/ssm/zerone-id/ap-northeast-2/jenkins_client_id.tf
+++ terraform/ssm/zerone-id/ap-northeast-2/jenkins_client_id.tf
@@ -1,9 +1,9 @@
-module "jenkins_client_id" {
-  source = "../../_modules/aws_ssm_parameter"
+# module "jenkins_client_id" {
+#   source = "../../_modules/aws_ssm_parameter"
 
-  ssm_parameter_name = "jenkins_client_id"
+#   ssm_parameter_name = "jenkins_client_id"
 
-ssm_parameter_key_id = data.terraform_remote_state.kms.outputs.aws_kms_key_apne2_deployment_common_arn
-ssm_parameter_value = local.encrypted_values.jenkins_client_id
+# ssm_parameter_key_id = data.terraform_remote_state.kms.outputs.aws_kms_key_apne2_deployment_common_arn
+# ssm_parameter_value = local.encrypted_values.jenkins_client_id
 
-}
+# }
