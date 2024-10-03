c01d6275037d66c131914a54febe2a04acf2e3c5
kuma.jung <yj.jung5078@gmail.com>
2024-10-03 13:36:24 +0900

    add deployment-common and sops kms

--- terraform/kms/zerone-id/ap-northeast-2/remote_state.tf
+++ terraform/kms/zerone-id/ap-northeast-2/remote_state.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "iam" {
     backend = "s3"
-    config = merge(var.remote_state.iam.zerone-id)
-    # config = merge(var.remote_state.iam.zerone-id, {"assume_role" = {"role_arn" = var.assume_role_arn}})
+    # config = merge(var.remote_state.iam.zerone-id)
+    config = merge(var.remote_state.iam.zerone-id, {"assume_role" = {"role_arn" = var.assume_role_arn}})
 }
 
 locals {
--- terraform/kms/zerone-id/ap-northeast-2/sops_zerone-id.tf
+++ terraform/kms/zerone-id/ap-northeast-2/sops_zerone-id.tf
@@ -1,21 +1,19 @@
 module "sops_zerone_id" {
   source       = "../../_modules/sops"
-  common_alias = "sops"
+  common_alias = "sops-zerone-id"
   common_aliow_arns = {
     manage = [
       "arn:aws:iam::${var.account_id.id}:root"
     ]
     use = [
-      "arn:aws:iam::${var.account_id.id}:root",
       local.remote_iam.sops_zerone_id_common_role_arn
     ]
     delete = ["arn:aws:iam::${var.account_id.id}:root"]
   }
-  secure_alias = "secure_sops"
+  secure_alias = "secure_sops-zerone-id"
   secure_aliow_arns = {
     manage = ["arn:aws:iam::${var.account_id.id}:root"]
     use = [
-      "arn:aws:iam::${var.account_id.id}:root",
       local.remote_iam.sops_zerone_id_secure_role_arn
     ]
     delete = ["arn:aws:iam::${var.account_id.id}:root"]
