7ea5ae1bbcc2d38359f0c0db40e82fe630a51e84
kuma.jung <yj.jung5078@gmail.com>
2024-10-03 12:07:23 +0900

    revert code

--- terraform/iam/zerone-id/devops_black.tf
+++ terraform/iam/zerone-id/devops_black.tf
@@ -1,22 +1,22 @@
 ############## DevOps Black Group ##################
-resource "aws_iam_group" "devops_black" {
-  name = "devops_black"
-}
+# resource "aws_iam_group" "devops_black" {
+#   name = "devops_black"
+# }
 
-resource "aws_iam_group_membership" "devops_black" {
-  name = aws_iam_group.devops_black.name
+# resource "aws_iam_group_membership" "devops_black" {
+#   name = aws_iam_group.devops_black.name
 
-  users = [
-    aws_iam_user.zerone.name,
-  ]
+#   users = [
+#     aws_iam_user.zerone.name,
+#   ]
 
-  group = aws_iam_group.devops_black.name
-}
-########### Managed Policies ####################
-resource "aws_iam_group_policy_attachment" "devops_black" {
-  group      = aws_iam_group.devops_black.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
+#   group = aws_iam_group.devops_black.name
+# }
+# ########### Managed Policies ####################
+# resource "aws_iam_group_policy_attachment" "devops_black" {
+#   group      = aws_iam_group.devops_black.name
+#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+# }
 ######################################################
 
 ############### Basic Policy ##################
--- terraform/iam/zerone-id/user_zerone.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_iam_user" "zerone" {
-  name = "zerone"
-  tags = {
-    "AKIAQ64UM3NIDEH5P654" = "local"
-    "AKIAQ64UM3NIPVW7YEET" = "terraform-master-class"
-  }
-}
