a5b9b69b3722917371b65bb2927eff433b3d9fb0
kuma.jung <yj.jung5078@gmail.com>
2024-10-03 10:49:03 +0900

    comment iam

--- terraform/iam/zerone-id/role_assume-zerone-id-admin-with-art-id.tf
+++ terraform/iam/zerone-id/role_assume-zerone-id-admin-with-art-id.tf
@@ -1,46 +1,47 @@
 #
 # zerone-id administrator
 #
-resource "aws_iam_role" "assume_zerone_id_admin" {
-  name                 = "assume-zerone-id-admin"
-  path                 = "/"
-  max_session_duration = "43200"
-  assume_role_policy   = data.aws_iam_policy_document.assume_zerone_id_admin_assume_role.json
-}
+# resource "aws_iam_role" "assume_zerone_id_admin" {
+#   name                 = "assume-zerone-id-admin"
+#   path                 = "/"
+#   max_session_duration = "43200"
+#   assume_role_policy   = data.aws_iam_policy_document.assume_zerone_id_admin_assume_role.json
+# }
 
-data "aws_iam_policy_document" "assume_zerone_id_admin_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
+# data "aws_iam_policy_document" "assume_zerone_id_admin_assume_role" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
+#     effect  = "Allow"
 
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::${var.account_id.id}:root",
-      "arn:aws:iam::${var.account_id.art-id}:root"]
-    }
-  }
-}
+#     principals {
+#       type = "AWS"
+#       identifiers = [
+#         "arn:aws:iam::${var.account_id.id}:root",
+#         "arn:aws:iam::${var.account_id.art-id}:root"
+#       ]
+#     }
+#   }
+# }
 
-resource "aws_iam_role_policy" "assume_zerone_id_admin_passrole" {
-  name = "assume-zerone-id-admin-passrole"
-  role = aws_iam_role.assume_zerone_id_admin.id
+# resource "aws_iam_role_policy" "assume_zerone_id_admin_passrole" {
+#   name = "assume-zerone-id-admin-passrole"
+#   role = aws_iam_role.assume_zerone_id_admin.id
 
-  policy = data.aws_iam_policy_document.assume_zerone_id_admin_pass_role.json
-}
+#   policy = data.aws_iam_policy_document.assume_zerone_id_admin_pass_role.json
+# }
 
-data "aws_iam_policy_document" "assume_zerone_id_admin_pass_role" {
-  statement {
-    actions = ["iam:PassRole"]
-    effect  = "Allow"
+# data "aws_iam_policy_document" "assume_zerone_id_admin_pass_role" {
+#   statement {
+#     actions = ["iam:PassRole"]
+#     effect  = "Allow"
 
-    resources = ["*"]
-  }
-}
+#     resources = ["*"]
+#   }
+# }
 
-resource "aws_iam_role_policy_attachment" "assume_zerone_id_admin" {
-  role       = aws_iam_role.assume_zerone_id_admin.id
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
+# resource "aws_iam_role_policy_attachment" "assume_zerone_id_admin" {
+#   role       = aws_iam_role.assume_zerone_id_admin.id
+#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+# }
 
 
--- terraform/iam/zerone-id/role_demo_tmcdapne2_task.tf
+++ terraform/iam/zerone-id/role_demo_tmcdapne2_task.tf
@@ -1,38 +1,38 @@
-resource "aws_iam_role" "demo_tmcdapne2_task" {
-  name               = "demo-tmcdapne2-task"
-  path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.demo_tmcdapne2_task_assume_role_document.json
-}
+# resource "aws_iam_role" "demo_tmcdapne2_task" {
+#   name               = "demo-tmcdapne2-task"
+#   path               = "/"
+#   assume_role_policy = data.aws_iam_policy_document.demo_tmcdapne2_task_assume_role_document.json
+# }
 
-data "aws_iam_policy_document" "demo_tmcdapne2_task_assume_role_document" {
-  statement {
-    effect = "Allow"
-    principals {
-      identifiers = [
-        "ecs-tasks.amazonaws.com"
-      ]
-      type = "Service"
-    }
-    actions = [
-      "sts:AssumeRole"
-    ]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceAccount"
-      values = [
-        "066346343248"
-      ]
-    }
-    condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values = [
-        "arn:aws:ecs:ap-northeast-2:066346343248:*"
-      ]
-    }
-  }
-}
+# data "aws_iam_policy_document" "demo_tmcdapne2_task_assume_role_document" {
+#   statement {
+#     effect = "Allow"
+#     principals {
+#       identifiers = [
+#         "ecs-tasks.amazonaws.com"
+#       ]
+#       type = "Service"
+#     }
+#     actions = [
+#       "sts:AssumeRole"
+#     ]
+#     condition {
+#       test     = "StringEquals"
+#       variable = "aws:SourceAccount"
+#       values = [
+#         "066346343248"
+#       ]
+#     }
+#     condition {
+#       test     = "ArnLike"
+#       variable = "aws:SourceArn"
+#       values = [
+#         "arn:aws:ecs:ap-northeast-2:066346343248:*"
+#       ]
+#     }
+#   }
+# }
 
-output "demo_tmcdapne2_task_arn" {
-  value = aws_iam_role.demo_tmcdapne2_task.arn
-}
+# output "demo_tmcdapne2_task_arn" {
+#   value = aws_iam_role.demo_tmcdapne2_task.arn
+# }
