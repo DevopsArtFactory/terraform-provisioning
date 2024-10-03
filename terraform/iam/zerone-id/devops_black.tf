7edffedab216fe0dcc063eb007cc48f5e6d3f538
kuma.jung <yj.jung5078@gmail.com>
2024-10-03 14:50:18 +0900

    update code

--- terraform/iam/zerone-id/devops_black.tf
+++ terraform/iam/zerone-id/devops_black.tf
@@ -24,19 +24,7 @@ resource "aws_iam_group_policy_attachment" "devops_black" {
 ######################################################
 
 ########### Assume Policies ####################
-resource "aws_iam_group_policy" "assume_role_policy" {
-  name   = "assume-role-policy"
-  group  = aws_iam_group.devops_black.name
-  policy = data.aws_iam_policy_document.assume_role_policy.json
-}
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
 
-    resources = ["arn:aws:iam::816736805842:role/assume-zerone-id-admin"]
-  }
-}
 ######################################################
 
 
