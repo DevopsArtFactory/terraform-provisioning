c384b41a27f8f23629a375f1431300a56ff25106
kuma.jung <yj.jung5078@gmail.com>
2024-10-06 12:29:20 +0900

    add github variable to terraform-provisionig

---
+++ github/terraform-provisioning/variables.tf
@@ -0,0 +1,15 @@
+variable "assume_role_arn" {
+  description = "The role to assume when accessing the AWS API."
+  default     = ""
+}
+
+variable "atlantis_user" {
+  description = "The username that will be triggering atlantis commands. This will be used to name the session when assuming a role. More information - https://github.com/runatlantis/atlantis#assume-role-session-names"
+  default     = "atlantis_user"
+}
+
+variable "owner" {
+  description = "GitHub owner used to configure the provider"
+  type        = string
+  default     = "DevopsArtFactory"
+}
