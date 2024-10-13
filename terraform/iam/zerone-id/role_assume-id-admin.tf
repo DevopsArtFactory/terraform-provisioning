2eff1afe89ffeb60864f80914816883553e2b195
kuma.jung <yj.jung5078@gmail.com>
2024-10-13 19:39:37 +0900

    add codebuild-deployrole and assume-id-admin role

---
+++ terraform/iam/zerone-id/role_assume-id-admin.tf
@@ -0,0 +1,46 @@
+#
+# zerone-id administrator
+#
+resource "aws_iam_role" "assume_id_admin" {
+  name                 = "assume-id-admin"
+  path                 = "/"
+  max_session_duration = "43200"
+  assume_role_policy   = data.aws_iam_policy_document.assume_id_admin_assume_role.json
+}
+
+data "aws_iam_policy_document" "assume_id_admin_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${var.account_id.id}:root",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "assume_id_admin_passrole" {
+  name = "assume-id-admin-passrole"
+  role = aws_iam_role.assume_id_admin.id
+
+  policy = data.aws_iam_policy_document.assume_id_admin_pass_role.json
+}
+
+data "aws_iam_policy_document" "assume_id_admin_pass_role" {
+  statement {
+    actions = ["iam:PassRole"]
+    effect  = "Allow"
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "assume_id_admin" {
+  role       = aws_iam_role.assume_id_admin.id
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+
