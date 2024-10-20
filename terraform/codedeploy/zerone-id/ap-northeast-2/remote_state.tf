f71bd8a5ecbb0d7e5b78ddc27f02d53896784350
kuma.jung <yj.jung5078@gmail.com>
2024-10-20 13:04:05 +0900

    Add demoapp codedeploy

--- terraform/codedeploy/zerone-id/ap-northeast-2/remote_state.tf
+++ terraform/codedeploy/zerone-id/ap-northeast-2/remote_state.tf
@@ -1,17 +1,17 @@
 data "terraform_remote_state" "iam" {
   backend = "s3"
-  config  = merge(var.remote_state.iam.zerone-id)
-  # config = merge(var.remote_state.iam.nginx.zerone-id), {"assume_role" = {"role_arn" = var.assume_role_arn}})
+  # config  = merge(var.remote_state.iam.zerone-id)
+  config = merge(var.remote_state.iam.zerone-id, {"assume_role" = {"role_arn" = var.assume_role_arn}})
 }
 
-data "terraform_remote_state" "nginx_tmcdapne2" {
-  backend = "s3"
-  config  = merge(var.remote_state.ecs.nginx.tmcdapne2)
-  # config = merge(var.remote_state.ecs.nginx.tmcdapne2), {"assume_role" = {"role_arn" = var.assume_role_arn}})
-}
+# data "terraform_remote_state" "nginx_tmcdapne2" {
+#   backend = "s3"
+#   config  = merge(var.remote_state.ecs.nginx.tmcdapne2)
+#   # config = merge(var.remote_state.ecs.nginx.tmcdapne2), {"assume_role" = {"role_arn" = var.assume_role_arn}})
+# }
 
 data "terraform_remote_state" "demo_tmcdapne2" {
   backend = "s3"
-  config  = merge(var.remote_state.ecs.demo.tmcdapne2)
-  # config = merge(var.remote_state.ecs.demo.tmcdapne2), {"assume_role" = {"role_arn" = var.assume_role_arn}})
+  # config  = merge(var.remote_state.ecs.demo.tmcdapne2)
+  config = merge(var.remote_state.ecs.demo.tmcdapne2, {"assume_role" = {"role_arn" = var.assume_role_arn}})
 }
