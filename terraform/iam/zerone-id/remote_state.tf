833097be7fb0a8316ce0ee81f403c25b8493150f
kuma.jung <yj.jung5078@gmail.com>
2024-10-20 13:14:26 +0900

    modify github action for codedeploy

--- terraform/iam/zerone-id/remote_state.tf
+++ terraform/iam/zerone-id/remote_state.tf
@@ -11,15 +11,15 @@ data "terraform_remote_state" "secretsmanager" {
 }
 
 
-# data "terraform_remote_state" "demo_tmcdapne2" {
-#   backend = "s3"
-#   config  = merge(var.remote_state.ecs.demo.tmcdapne2)
-#   # config = merge(var.remote_state.ecs.demo.tmcdapne2, {"assume_role" = {"role_arn" = var.assume_role_arn}})
-# }
+data "terraform_remote_state" "demo_tmcdapne2" {
+  backend = "s3"
+  #   config  = merge(var.remote_state.ecs.demo.tmcdapne2)
+  config = merge(var.remote_state.ecs.demo.tmcdapne2, { "assume_role" = { "role_arn" = var.assume_role_arn } })
+}
 
 
-# data "terraform_remote_state" "codedeploy" {
-#   backend = "s3"
-#   config  = merge(var.remote_state.codedeploy.zerone-id.apne2)
-#   # config = merge(var.remote_state.codedeploy.zerone-id.apne2, {"assume_role" = {"role_arn" = var.assume_role_arn}})
-# }
+data "terraform_remote_state" "codedeploy" {
+  backend = "s3"
+  #   config  = merge(var.remote_state.codedeploy.zerone-id.apne2)
+  config = merge(var.remote_state.codedeploy.zerone-id.apne2, { "assume_role" = { "role_arn" = var.assume_role_arn } })
+}
