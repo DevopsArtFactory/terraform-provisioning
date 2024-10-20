d18217981107b257f093d69ac9f479049dedf09b
kuma.jung <yj.jung5078@gmail.com>
2024-10-20 12:18:27 +0900

    add ecs task execution role and task role

--- terraform/iam/zerone-id/role_demo_tmcdapne2_task.tf
+++ terraform/iam/zerone-id/role_demo_tmcdapne2_task.tf
@@ -1,68 +1,68 @@
-# resource "aws_iam_role" "demo_tmcdapne2_task" {
-#   name               = "demo-tmcdapne2-task"
-#   path               = "/"
-#   assume_role_policy = data.aws_iam_policy_document.demo_tmcdapne2_task_assume_role_document.json
-# }
+resource "aws_iam_role" "demo_tmcdapne2_task" {
+  name               = "demo-tmcdapne2-task"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.demo_tmcdapne2_task_assume_role_document.json
+}
 
-# data "aws_iam_policy_document" "demo_tmcdapne2_task_assume_role_document" {
-#   statement {
-#     effect = "Allow"
-#     principals {
-#       identifiers = [
-#         "ecs-tasks.amazonaws.com"
-#       ]
-#       type = "Service"
-#     }
-#     actions = [
-#       "sts:AssumeRole"
-#     ]
-#     condition {
-#       test     = "StringEquals"
-#       variable = "aws:SourceAccount"
-#       values = [
-#         "066346343248"
-#       ]
-#     }
-#     condition {
-#       test     = "ArnLike"
-#       variable = "aws:SourceArn"
-#       values = [
-#         "arn:aws:ecs:ap-northeast-2:066346343248:*"
-#       ]
-#     }
-#   }
-# }
+data "aws_iam_policy_document" "demo_tmcdapne2_task_assume_role_document" {
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = [
+        "ecs-tasks.amazonaws.com"
+      ]
+      type = "Service"
+    }
+    actions = [
+      "sts:AssumeRole"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values = [
+        "066346343248"
+      ]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values = [
+        "arn:aws:ecs:ap-northeast-2:066346343248:*"
+      ]
+    }
+  }
+}
 
-# resource "aws_iam_role_policy" "demo_kms" {
-#   name   = "demo-kms-decryption"
-#   role   = aws_iam_role.demo_tmcdapne2_task.id
-#   policy = data.aws_iam_policy_document.demo_kms_ssm_document.json
-# }
+resource "aws_iam_role_policy" "demo_kms" {
+  name   = "demo-kms-decryption"
+  role   = aws_iam_role.demo_tmcdapne2_task.id
+  policy = data.aws_iam_policy_document.demo_kms_ssm_document.json
+}
 
-# data "aws_iam_policy_document" "demo_kms_ssm_document" {
-#   statement {
-#     sid    = "AllowToDecryptKMSKey"
-#     effect = "Allow"
-#     resources = [
-#       data.terraform_remote_state.kms.outputs.aws_kms_key_apne2_deployment_common_arn
-#     ]
-#     actions = [
-#       "kms:Decrypt"
-#     ]
-#   }
+data "aws_iam_policy_document" "demo_kms_ssm_document" {
+  statement {
+    sid    = "AllowToDecryptKMSKey"
+    effect = "Allow"
+    resources = [
+      data.terraform_remote_state.kms.outputs.aws_kms_key_apne2_deployment_common_arn
+    ]
+    actions = [
+      "kms:Decrypt"
+    ]
+  }
 
-#   statement {
-#     sid    = "AllowSecretsManagerAccess"
-#     effect = "Allow"
-#     resources = [
-#       "arn:aws:secretsmanager:ap-northeast-2:${var.account_id.id}:secret:/secret/demo-4n44r2"
-#     ]
-#     actions = [
-#       "secretsmanager:GetSecretValue"
-#     ]
-#   }
-# }
+  statement {
+    sid    = "AllowSecretsManagerAccess"
+    effect = "Allow"
+    resources = [
+      data.terraform_remote_state.secretsmanager.outputs.aws_secretsmanager_demo_tmcdapne2_id
+    ]
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+  }
+}
 
-# output "demo_tmcdapne2_task_arn" {
-#   value = aws_iam_role.demo_tmcdapne2_task.arn
-# }
+output "demo_tmcdapne2_task_arn" {
+  value = aws_iam_role.demo_tmcdapne2_task.arn
+}
--- terraform/iam/zerone-id/role_demo_tmcdapne2_task_exec.tf
+++ terraform/iam/zerone-id/role_demo_tmcdapne2_task_exec.tf
@@ -1,55 +1,55 @@
-# resource "aws_iam_role" "demo_tmcdapne2_task_exec" {
-#   name               = "demo-tmcdapne2-task-exec"
-#   path               = "/"
-#   assume_role_policy = data.aws_iam_policy_document.demo_tmcdapne2_task_exec_assume_role_document.json
-# }
+resource "aws_iam_role" "demo_tmcdapne2_task_exec" {
+  name               = "demo-tmcdapne2-task-exec"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.demo_tmcdapne2_task_exec_assume_role_document.json
+}
 
-# data "aws_iam_policy_document" "demo_tmcdapne2_task_exec_assume_role_document" {
-#   statement {
-#     effect = "Allow"
-#     principals {
-#       identifiers = [
-#         "ecs-tasks.amazonaws.com"
-#       ]
-#       type = "Service"
-#     }
-#     actions = [
-#       "sts:AssumeRole"
-#     ]
-#   }
-# }
+data "aws_iam_policy_document" "demo_tmcdapne2_task_exec_assume_role_document" {
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = [
+        "ecs-tasks.amazonaws.com"
+      ]
+      type = "Service"
+    }
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
 
-# resource "aws_iam_role_policy" "demo_tmcdapne2_task_exec_policy" {
-#   name   = "task_exec_policy"
-#   role   = aws_iam_role.demo_tmcdapne2_task_exec.id
-#   policy = data.aws_iam_policy_document.demo_tmcdapne2_task_exec_policy.json
+resource "aws_iam_role_policy" "demo_tmcdapne2_task_exec_policy" {
+  name   = "task_exec_policy"
+  role   = aws_iam_role.demo_tmcdapne2_task_exec.id
+  policy = data.aws_iam_policy_document.demo_tmcdapne2_task_exec_policy.json
 
-# }
+}
 
-# data "aws_iam_policy_document" "demo_tmcdapne2_task_exec_policy" {
-#   statement {
-#     sid    = "ECR"
-#     effect = "Allow"
-#     actions = [
-#       "ecr:GetDownloadUrlForLayer",
-#       "ecr:GetAuthorizationToken",
-#       "ecr:BatchGetImage",
-#       "ecr:BatchCheckLayerAvailability"
-#     ]
-#     resources = ["*"]
-#   }
+data "aws_iam_policy_document" "demo_tmcdapne2_task_exec_policy" {
+  statement {
+    sid    = "ECR"
+    effect = "Allow"
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability"
+    ]
+    resources = ["*"]
+  }
 
-#   statement {
-#     sid    = "Log"
-#     effect = "Allow"
-#     actions = [
-#       "logs:PutLogEvents",
-#       "logs:CreateLogStream"
-#     ]
-#     resources = ["*"]
-#   }
-# }
+  statement {
+    sid    = "Log"
+    effect = "Allow"
+    actions = [
+      "logs:PutLogEvents",
+      "logs:CreateLogStream"
+    ]
+    resources = ["*"]
+  }
+}
 
-# output "demo_tmcdapne2_task_exec_arn" {
-#   value = aws_iam_role.demo_tmcdapne2_task_exec.arn
-# }
+output "demo_tmcdapne2_task_exec_arn" {
+  value = aws_iam_role.demo_tmcdapne2_task_exec.arn
+}
