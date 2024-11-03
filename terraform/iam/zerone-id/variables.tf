3434d76d7d8ffa2b0fa87d14a5abf44d43f67edc
kuma.jung <yj.jung5078@gmail.com>
2024-11-03 20:43:50 +0900

    modify datadog role external id

--- terraform/iam/zerone-id/variables.tf
+++ terraform/iam/zerone-id/variables.tf
@@ -5,5 +5,5 @@ variable "aws_region" {
 
 variable "datadog_external_id" {
   description = "Datadog External Id"
-  default     = "464622532012"
+  default     = "3c42947d62d2431d95c1e41a1ecfeb0c"
 }
