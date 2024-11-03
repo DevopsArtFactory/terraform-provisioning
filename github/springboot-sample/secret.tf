424a8324f9e68a1507e458b94e1c7283da5dba71
kuma.jung <yj.jung5078@gmail.com>
2024-11-03 18:10:52 +0900

    add infr github token

--- github/springboot-sample/secret.tf
+++ github/springboot-sample/secret.tf
@@ -15,4 +15,13 @@ module "springboot_sample_aws_account_num" {
 }
 
 
+module "springboot_sample_infra_github_token" {
+  source        = "../_module/action"
+  repository    = local.repository_name
+  secret_name   = "INFRA_GITHUB_TOKEN"
+  owner         = var.owner
+  secret_string = data.sops_file.springboot_sample.data["infra_github_token"]
+}
+
+
 
