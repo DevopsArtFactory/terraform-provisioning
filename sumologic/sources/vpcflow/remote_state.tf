fa149a7fd5116e406afd0c004a795f78816a9e57
kuma.jung <yj.jung5078@gmail.com>
2024-11-08 18:05:46 +0900

    add source for vpcflowlog

--- sumologic/sources/vpcflow/remote_state.tf
+++ sumologic/sources/vpcflow/remote_state.tf
@@ -1,5 +1,5 @@
 data "terraform_remote_state" "sumologic_collector" {
   backend = "s3"
 
-  config = merge(var.remote_state.sumologic.collector, {"assume_role_arn" = { "role_arn" = var.assume_role_arn }})
+  config = merge(var.remote_state.collector, {"assume_role" = { "role_arn" = var.assume_role_arn }})
 }
