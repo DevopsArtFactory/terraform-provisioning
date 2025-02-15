ef68420dd2db6b2c3df273bea93f441239f1ae2f
kuma.jung <yj.jung5078@gmail.com>
2025-02-15 13:23:33 +0900

    add kms

---
+++ terraform/kms/devart-id/ap-northeast-2/deployment_common.tf
@@ -0,0 +1,27 @@
+resource "aws_kms_key" "test" {
+  description         = "KMS key for common secrets in ${var.aws_region}."
+  enable_key_rotation = true
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Id" : "test",
+    "Statement" : [
+      {
+        "Sid" : "Enable IAM User Permissions",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : [
+            "arn:aws:iam::${var.account_id.id}:root"
+          ]
+        },
+        "Action" : "kms:*",
+        "Resource" : "*"
+      }
+    ]
+  })
+
+}
+
+resource "aws_kms_alias" "test_kms_alias" {
+  name          = "alias/test"
+  target_key_id = aws_kms_key.test.key_id
+}
---
+++ terraform/kms/devart-id/outputs.tf
@@ -0,0 +1,5 @@
+output "aws_kms_key_apne2_deployment_common_arn" {
+  description = "deployment kms id"
+  value       = aws_kms_key.deployment_common.arn
+}
+
